aff5d7c68b7da70b28c0b6bacefde5bc048894c3
kje031417 <kje031417@gmail.com>
2023-04-12 17:20:06 +0900

    23.04.12 Initial commit

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,7 @@
+package hello.login.web;
+
+public interface SessionConst {    //추상클래스 or 인터페이스로 사용
+
+    //public static final String LOGIN_MEMBER = "loginMember";
+    String LOGIN_MEMBER = "loginMember";
+}
